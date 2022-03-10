79ffc553b8e04638940ae15243c95aa5ac8a2b83
Khasan <ixanaxy@gmail.com>
2022-03-10 16:37:55 +0500

    Initial Commit

---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by Xasan Xasanov on 10/03/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
